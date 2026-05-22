e8dbe4b5247adfa48b5220f61966da753bff9ee7
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2026-05-22 23:25:06 +0530

    Number Program

---
+++ src/Loops_In_Java/While_loop/Factorial.java
@@ -0,0 +1,22 @@
+package Loops_In_Java.While_loop;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number : ");
+		int n = sc.nextInt();
+
+		int fact = 1;
+		int i = 1;
+		while (i <= n) {
+
+			fact = fact * i;
+			i++;
+		}
+		System.out.println(n + "! = : " + fact);
+	}
+}
---
+++ src/Loops_In_Java/While_loop/Sum1.java
@@ -0,0 +1,15 @@
+package Loops_In_Java.While_loop;
+
+public class Sum1 {
+
+	public static void main(String[] args) {
+
+		double sum = 0;
+		int i = 1;
+		while (i <= 100) {
+			sum = sum + (1.0 / i);
+			i++;
+		}
+		System.out.println("Sum is " + sum);
+	}
+}
