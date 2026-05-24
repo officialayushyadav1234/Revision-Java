4185462e0b6eb6a3d20c9c8bf9c5da3095da5883
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2026-05-24 23:54:24 +0530

    Do while Loop

---
+++ src/Loops_In_Java/Do_while_loop/Program1.java
@@ -0,0 +1,15 @@
+package Loops_In_Java.Do_while_loop;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Program start");
+		int i = 1;
+		do {
+			System.out.println("loop Body : " + i);
+			i++;
+		} while (i >= 100);
+		System.out.println("Program Ends !! ");
+	}
+}
---
+++ src/Loops_In_Java/Do_while_loop/Program2.java
@@ -0,0 +1,15 @@
+package Loops_In_Java.Do_while_loop;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Program Start");
+		int i = 1;
+		do {
+			System.out.println("Loop body : " + i);
+			i++;
+		} while (i <= 10);
+		System.out.println("Ends Program !! ");
+	}
+}
