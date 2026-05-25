3d743c40e23907941905c461415da12bae5182ff
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2026-05-25 23:40:00 +0530

    Loops

---
+++ src/Loops_In_Java/Do_while_loop/Factorial.java
@@ -0,0 +1,27 @@
+package Loops_In_Java.Do_while_loop;
+
+import java.util.Scanner;
+
+public class Factorial {
+   
+public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		char c;
+		do{
+			System.out.println("ENter the Number: ");
+			int n=sc.nextInt();
+			int fact=1;
+			int i=1;
+			while(i<=n)
+			{
+				fact=fact*i;
+				i++;
+			}
+			System.out.println(n+"!= "+fact);
+			System.out.println("Press Y/y to continue or any oth. char. to STOP!");
+			c=sc.next().charAt(0);
+		}while(c=='Y' || c=='y');
+		System.out.println("Thank You!Program ENDS NOW!!");
+	}
+}
