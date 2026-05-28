67621a68d2247e01bf5300911affc6a0c41478ad
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2026-05-28 23:17:12 +0530

    Methods

---
+++ src/Method/Program5.java
@@ -0,0 +1,23 @@
+package Method;
+
+import java.util.Scanner;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		getReverse();
+	}
+
+	public static void getReverse() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int n = sc.nextInt();
+		int rev = 0;
+		while (n > 0) {
+			int rem = n % 10;
+			rev = rev * 10 + rem;
+			n = n / 10;
+		}
+		System.out.println("The Reverse value is:" + rev);
+	}
+}
