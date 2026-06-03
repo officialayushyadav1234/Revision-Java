e09b456e0b00083d45a000709f6ea295ff292540
officialayushyadav1234 <officialayushyadav1234@gmail.com>
2026-06-03 23:55:21 +0530

    Oops Concept

---
+++ src/Oops_Concept/Static_block/Program3.java
@@ -0,0 +1,14 @@
+package Oops_Concept.Static_block;
+
+public class Program3 {
+
+	static {
+		System.out.println("This is static block-1 ");
+
+	}
+
+	static {
+		System.out.println("This is static block-2 ");
+
+	}
+}
